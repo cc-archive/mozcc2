172d5d734e5cd53ab83ad0e56dd2fd47bbdbe47a
nyergler <nyergler@committer.creativecommons.org>
2006-11-14 17:25:51 +0000

    Added license header; call the storage opener on startup so our columns are present for the first run.

--- chrome/content/birdfeed.js
+++ chrome/content/birdfeed.js
@@ -1,3 +1,29 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is MozCC 2.
+ *
+ * The Initial Developer of the Original Code is
+ * Nathan R. Yergler, Creative Commons.
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Nathan R. Yergler <nathan@creativecommons.org>
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 function mozcc_webplaylist_observer() {
 
     // observer for changes to the webplaylist.current attribute
@@ -38,9 +64,11 @@ function mozcc_initialize() {
     var remote = SB_NewDataRemote("webplaylist.current", null);
     remote.bindObserver(new mozcc_webplaylist_observer(), true);
 
+
+    // call storage.open so we add our columns right away
+    getStorage().open();
+
 } // mozcc_initialize
 
-
-alert('foo');
-
 mozcc_initialize();
+
