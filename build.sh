87cbce5cf4db3d302a798c329e575f62d4874041
nyergler <nyergler@committer.creativecommons.org>
2007-02-20 01:14:14 +0000

    Build script for the bundle, install.rdf updates to distinguish it from the individual install manifests.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# Bundle Build Script for MozCC
+# (c) 2007, Nathan R. Yergler, Creative Commons
+# Licensed to the public; see LICENSE for details
+
+# create XPI files for individual pieces
+cd metacore
+zip -r ../metacore.xpi *
+cd ..
+
+# create the bundle
+zip -r mozcc.xpi install.rdf metacore.xpi 
